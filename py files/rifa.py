adb9d15c99cb81832f51fcc10feee0e32132139f
Niloy Niil <22797857+niloyniil@users.noreply.github.com>
2019-12-10 12:53:26 +0600

    Add files via upload

--- py files/rifa.py
+++ py files/rifa.py
@@ -59,11 +59,54 @@ df['class'] = [1 if x == '<=50K' else 0 for x in df['class']]
 print(df['class'].value_counts())
 
 #droping column 0-> rows 1->column
-y=df.drop('class',1)
+X=df.drop('class',1)
 print(y.head(5))
-z=df['class']
+Y=df['class']
 
 #column names
 print(df.columns)
 
-print(df.education.value_counts)s
+#how many data in each category of education column
+print(df.education.value_counts)
+
+#oe hot encoding
+df['education']=pd.get_dummies(X['education'])
+
+print(pd.get_dummies(X['education']).head(5))
+
+#How many uique categories
+len(X['education'].unique())
+print(X.colums)
+for col_name in X.columns:
+    if(X[col_name].dtypes == 'object'):
+        unique_cat = len(X[col_name].unique())
+        #print()
+        print(col_name,unique_cat)
+        
+X['native-country'].value_counts().sort_values(ascending=False).head(10)
+X['native-country']=['United-States']=['United States' if x=='United-States' else 'others' for x in X['native-country']]
+todummy_list=['workclass','education','marial-status','occupation','relationship','race','sex','native-country']
+
+def dummy_df(df,todummy_list):
+    for x in todummy_list:
+        dummies =pd.get_dummies=pd.get_dummies(df[x],prefix=x,dummy_na =False)
+        df = df.drop(x,1)
+        df = pd.concat([df,dummies],axis=1)
+        return  df
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
